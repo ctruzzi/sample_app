a1ae159681e03a4ffd011dcc97bba50748bee03f
Craig Truzzi <lothia@gmail.com>
2012-05-08 16:59:38 -0700

    User model with password, and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
+require 'digest'
+
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
+	attr_accessor :password
+	attr_accessible :name, :email, :password, :password_confirmation
 
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -9,5 +12,43 @@ class User < ActiveRecord::Base
 	validates :email,	:presence => true,
 						:format => { :with => email_regex },
 						:uniqueness => { :case_sensitive => false }
+	validates :password, :presence => true,
+						:confirmation => true,
+						:length => { :within => 6..40 }					
 								
+	#Call encrypt_password method prior to saving in active record
+	before_save :encrypt_password
+
+	#is self. so we can invoke directly on User
+	def self.authenticate(email, submitted_password)
+		user = find_by_email(email)
+		return nil if user.nil? #return nil if we cannot find user
+		return user if user.has_password?(submitted_password)
+		#returns nil if doesn't meet either
+	end
+
+	# Return true if the user's password matches the submitted password.
+	def has_password?(submitted_password)
+		# Compare encrypted_password with the encrypted version of submitted_password.
+		encrypted_password == encrypt(submitted_password)
+	end
+
+	private
+
+		def encrypt_password
+			self.salt = make_salt if new_record?
+			self.encrypted_password = encrypt(password)
+		end
+
+		def encrypt(string)
+			secure_hash("#{salt}--#{string}")
+		end
+
+		def make_salt
+			secure_hash("#{Time.now.utc}--#{password}")
+		end
+
+		def secure_hash(string)
+			Digest::SHA2.hexdigest(string)
+		end
 end
